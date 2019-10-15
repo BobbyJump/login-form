ab6d27406301f6c2889043e97344e6099ac01f57
Dmitriy <skachok.d@gmail.com>
2019-10-15 23:14:03 +0300

    Updates

---
+++ precache-manifest.02ce10b727869bfe8bb68c805d41a3c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db5e95a7d81b8b40d384c2c9ed4f51f",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "b9c5923ddec476adb714",
+    "url": "/login-form/static/css/main.2de7c5b7.chunk.css"
+  },
+  {
+    "revision": "54fe38f272bed8617306",
+    "url": "/login-form/static/js/2.638896bb.chunk.js"
+  },
+  {
+    "revision": "b9c5923ddec476adb714",
+    "url": "/login-form/static/js/main.02b40472.chunk.js"
+  },
+  {
+    "revision": "ea1eb0d035bd58e2f582",
+    "url": "/login-form/static/js/runtime-main.0fb4e128.js"
+  }
+]);
