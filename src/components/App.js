f6baf97ff77b1a69d36d00493f9c43de12bacca5
Dmitriy <skachok.d@gmail.com>
2019-10-15 23:05:56 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,166 @@
+import React from "react";
+import countries from "../data/countries"
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      username: "",
+      password: "",
+      repeatPassword: "",
+      country: "1",
+      gender: "male",
+      agree: true
+    };
+  }
+
+  onChange = event => {
+    console.log(event.target.name, event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onChangeAgree = event => {
+    //debugging
+    console.log(event.target.name, event.target.value, event.target.checked);
+    console.log(typeof event.target.value)
+    this.setState({
+      [event.target.name]: event.target.checked
+      // [event.target.name]: event.target.value - not working
+      // but, another way
+      // [event.target.name]: event.target.value === "true" ? false : true
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    // console.log("refs", this.username.value, this.password.value);
+    console.log(this.state);
+  };
+
+  getItemsOptions = items =>{
+    return items.map(item =>(
+      <option key={item.id} value={item.id}>
+        {item.name}
+      </option>
+    ));
+  };
+
+  render() {
+    // const getCountriesOptions = countries.map(country =>(
+    //   <option key={country.id} value={country.id}>
+    //     {country.name}
+    //   </option>
+    // ));
+    
+    //console.log(this);
+    return (
+      <div className="form-container card">
+        <form className="form card-body">
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter username"
+              ref={node => (this.username = node)}
+              name="username"
+              value = {this.state.username}
+              onChange = {this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter password"
+              ref={node => (this.password = node)}
+              name="password"
+              value = {this.state.password}
+              onChange = {this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Repeat password</label>
+            <input
+              type = "text"
+              className = "form-control"
+              placeholder = "Enter repeat password"
+              ref = {node => (this.repeatPassword = node)}
+              name = "repeatPassword"
+              value = {this.state.repeatPassword}
+              onChange = {this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="country">Country</label>
+            <select 
+              className = "form-control" 
+              id = "country"
+              name ="country"
+              value = {this.state.country}
+              onChange = {this.onChange}
+            >
+              {this.getItemsOptions(countries)}
+            </select>
+          </div>
+          <fieldset className="form-group">
+            <div>Gender</div>
+            <div className="form-check">
+              <input 
+                className="form-check-input" 
+                type="radio" 
+                id="male" 
+                name="gender"
+                value="male"
+                checked={this.state.gender === "male"}
+                onChange={this.onChange}
+              />
+              <label className="form-check-label" htmlFor="male">
+                Male
+              </label>
+            </div>
+            <div className="form-check">
+              <input 
+                className="form-check-input" 
+                type="radio" 
+                id="female" 
+                name="gender"
+                value="female"
+                checked={this.state.gender === "female"}
+                onChange={this.onChange}
+              />
+              <label className="form-check-label" htmlFor="female">
+                Female
+              </label>
+            </div>
+          </fieldset>
+          <div className="form-check">
+            <input 
+              className="form-check-input" 
+              type="checkbox" 
+              id="agree" 
+              name="agree"
+              value={this.state.agree}
+              onChange={this.onChangeAgree}
+              checked={this.state.agree}
+            />
+            <label className="form-check-label" htmlFor="agree">
+              Agree
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary w-100"
+            onClick={this.onSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
